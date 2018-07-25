49708de753e481dc2ede797d86158a7ca08e426d
monil <moniljain17497@gmail.com>
2018-07-25 15:06:51 +0530

    Initial Commit

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+urlpatterns=[
+	url(r'^posts/$',views.post_list,name='post_list'),
+	url(r'^posts/(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
+	url(r'^posts/new/$',views.post_new,name='post_new'),
+	url(r'^posts/(?P<pk>\d+)/edit/$',views.post_edit,name='post_edit'),
+	url(r'^posts/draft/$',views.post_draft_list,name='post_draft_list'),
+	url(r'^posts/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+	url(r'^posts/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+	url(r'^posts/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+	url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
+]
